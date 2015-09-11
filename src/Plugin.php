b05028ab3732e6c672d57e51e52013295ba93696
Barry Kooij <git@barrykooij.nl>
2015-09-11 21:05:05 +0200

    Only setup plugin in admin

--- src/Plugin.php
+++ src/Plugin.php
@@ -29,29 +29,33 @@ class Plugin extends Pimple\Container {
 		// register services early since some add-ons need 'm
 		$this->register_services();
 
-		// alias for Pimple container
-		$c = $this;
+		// this complete plugin is pretty much admin only
+		if ( is_admin() ) {
+			// alias for Pimple container
+			$c = $this;
 
-		// enqueue CSS
-		add_action( 'admin_enqueue_scripts', function () use ( $c ) {
-			wp_enqueue_style(
-				'wpcm_admin',
-				$c['file']->plugin_url( '/assets/css/wp-notification-center.css' ),
-				array(),
-				$c->get_version()
-			);
-		} );
+			// enqueue CSS
+			add_action( 'admin_enqueue_scripts', function () use ( $c ) {
+				wp_enqueue_style(
+					'wpcm_admin',
+					$c['file']->plugin_url( '/assets/css/wp-notification-center.css' ),
+					array(),
+					$c->get_version()
+				);
+			} );
 
-		// catch admin notices
-		$this['admin_notice_handler']->catch_admin_notices();
+			// catch admin notices
+			$this['admin_notice_handler']->catch_admin_notices();
 
-		// setup admin bar
-		$admin_bar = new AdminBar();
-		$admin_bar->setup();
+			// setup admin bar
+			$admin_bar = new AdminBar();
+			$admin_bar->setup();
 
-		// setup plugin links
-		$plugin_links = new PluginLinks();
-		$plugin_links->setup();
+			// setup plugin links
+			$plugin_links = new PluginLinks();
+			$plugin_links->setup();
+		}
+		
 	}
 
 	/**
